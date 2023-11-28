11fd6c226cf11afb83d118f685d9e4d24ad16ccd
Eric Wittmann <eric.wittmann@gmail.com>
2023-11-28 10:05:48 -0500

    Building blocks and workflows for react component library

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        dts({ include: ["lib"] })
+    ],
+    build: {
+        copyPublicDir: false,
+        lib: {
+            fileName: "main",
+            entry: resolve(__dirname, "lib/main.ts"),
+            formats: ["es"]
+        },
+        rollupOptions: {
+            external: [
+                "react",
+                "react-dom",
+                "react/jsx-runtime",
+                "@patternfly/patternfly",
+                "@patternfly/react-core",
+                "@patternfly/react-icons",
+                "@patternfly/react-table"
+            ]
+        }
+    }
+});
